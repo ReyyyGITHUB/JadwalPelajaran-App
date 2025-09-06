be17b7318873a0f43167337e5f59c61919bffd45
ReyyyGITHUB <emailkamu@gmail.com>
2025-09-06 20:49:36 +0700

    Jadwal XPPLG2 2.0.9 / Added iOS, Android, & Desktop friendly

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Jadwal Pelajaran X PPLG 2',
+        short_name: 'Jadwal',
+        description: 'Aplikasi jadwal pelajaran berbasis web',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: './',
+        icons: [
+          {
+            src: '/icons/logo2.0.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/logo2.0.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ],
 })
