6cac8556c6d4a0f86b1ccd308529f804694808b9
ReyyyGITHUB <emailkamu@gmail.com>
2025-10-04 22:32:11 +0700

    update notifikasi popup peringatan

--- src/JadwalSnapan.jsx
+++ src/JadwalSnapan.jsx
@@ -1,8 +1,16 @@
+'use client'; // optional kalau kamu pakai Next.js, tapi di React biasa nggak perlu
+
 import Home from "./pages/Home";
+import WeeklyNotificationPopup from "./components/WeeklyNotificationPopup"; 
+// pastiin path-nya sesuai, misal ./components/WeeklyNotificationPopup.jsx
 
 export default function JadwalSnapan() {
   return (
     <>
+      {/* Popup ditaruh di sini supaya muncul di atas seluruh konten halaman */}
+      <WeeklyNotificationPopup hour={5} minute={0} />
+
+      {/* Konten utama halaman */}
       <Home />
     </>
   );
